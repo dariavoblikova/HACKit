de5a742b58f3be5228c374a111d49beb9918f7a6
Daria Voblikova <daria.voblikova@gmail.com>
2020-04-19 11:45:49 -0700

    added Olena's graphs and adjusted design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HACKit Team' });
+  res.render('index', { title: 'HACK it Team' });
 });
 
 module.exports = router;
